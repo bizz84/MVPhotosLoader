e8bf5744c1eda88b245693dfd9ea7908c59f7c62
Andrea Bizzotto <bizz84@gmail.com>
2016-03-06 17:00:50 +0000

    Initial Commit

---
+++ MVPhotosLoader/MVPhotosLoader.h
@@ -0,0 +1,19 @@
+//
+//  MVPhotosLoader.h
+//  MVPhotosLoader
+//
+//  Created by Andrea Bizzotto on 06/03/2016.
+//  Copyright © 2016 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVPhotosLoader.
+FOUNDATION_EXPORT double MVPhotosLoaderVersionNumber;
+
+//! Project version string for MVPhotosLoader.
+FOUNDATION_EXPORT const unsigned char MVPhotosLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVPhotosLoader/PublicHeader.h>
+
+
