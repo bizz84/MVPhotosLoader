85b1c4c6cbd0efafc720fd74558d266b643a925d
Andrea Bizzotto <bizz84@gmail.com>
2016-03-07 07:32:47 +0000

    Working album generation and loading

--- MVPhotosLoader/MVPhotosLoader.swift
+++ MVPhotosLoader/MVPhotosLoader.swift
@@ -37,36 +37,115 @@ public class MVPhotosLoader: NSObject {
         return fetchResult.firstObject as? PHAssetCollection
     }
     
+    private class func addMissingAlbums(assetSources: [MVAssetSourceMetadata], existingAlbums: [PHAssetCollection], completion: (error: NSError?) -> ()) {
+
+        let allAlbumNames = Set(assetSources.flatMap { $0.albums })
+        
+        let missingNames = missingAlbumNames(fromNames: allAlbumNames, assetCollections: existingAlbums)
+        
+        if missingNames.count > 0 {
+        
+            PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+            
+                let _ = createAlbums(names: missingNames)
+
+                }) { success, error  in
+                    
+                    completion(error: error)
+            }
+        }
+        else {
+            completion(error: nil)
+        }
+    }
+    
     private class func addPhotos(assetSources: [MVAssetSourceMetadata], toAssetCollection assetCollection: PHAssetCollection?, completion: (error: NSError?) -> ()) {
         
-        //        let data = NSData(contentsOfFile: "a.jpg")
-        //        if
+        let userAssetCollections = fetchTopLevelUserCollections()
         
-        //        let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
-        //        let assetPlaceholder = createAssetRequest.placeholderForCreatedAsset
-        //        let albumChangeRequest = PHAssetCollectionChangeRequest(forAssetCollection: album)
-        //        albumChangeRequest.addAssets([assetPlaceholder])
+        addMissingAlbums(assetSources, existingAlbums: userAssetCollections) { error in
+            
+            let userAssetCollections = fetchTopLevelUserCollections()
+            
+            PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                
+                for assetSource in assetSources {
+                    
+                    insertAsset(assetSource, inAssetCollections: userAssetCollections)
+                }
+                
+                }) { success, error  in
+                    
+                    completion(error: error)
+            }
+        }
+    }
+    
+    
+    class func missingAlbumNames(fromNames names: Set<String>, assetCollections: [PHAssetCollection]) -> [String] {
+
+        let existingAlbumNames = assetCollections.flatMap{ $0.localizedTitle }
+
+        var namesToAdd: [String] = []
+        for name in names {
+            
+            if !existingAlbumNames.contains(name) {
+                
+                namesToAdd.append(name)
+            }
+        }
+        return namesToAdd
+    }
+    
+    class func createAlbums(names names: [String]) -> [PHAssetCollectionChangeRequest] {
         
-        PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+        return names.map { PHAssetCollectionChangeRequest.creationRequestForAssetCollectionWithTitle($0) }
+    }
+    
+    class func insertAsset(metadata: MVAssetSourceMetadata, inAssetCollections assetCollections: [PHAssetCollection]) {
+     
+        guard let changeRequest = PHAssetChangeRequest.creationRequestForAssetFromImageAtFileURL(metadata.fileURL) else {
+            return
+        }
+        
+        let filteredAssetCollections = filterAssetCollections(assetCollections, forAlbumsInMetadata: metadata)
+        
+        if let placeholder = changeRequest.placeholderForCreatedAsset {
             
-            //let nonInsertedFilesURLs = filesURLs.filter { /* on json */ }
-            //            let images = filesURLs.flatMap{ $0.path }.flatMap{ UIImage(contentsOfFile: $0 ) }
-            //            let assetChangeRequests = images.flatMap{ PHAssetChangeRequest.creationRequestForAssetFromImage( $0 ) }
-            let assetChangeRequests = assetSources.flatMap { PHAssetChangeRequest.creationRequestForAssetFromImageAtFileURL($0.fileURL) }
-            
-            if let assetCollection = assetCollection {
-                let placeholders = assetChangeRequests.flatMap{ $0.placeholderForCreatedAsset }
+            for assetCollection in filteredAssetCollections {
                 
                 if let assetCollectionChangeRequest = PHAssetCollectionChangeRequest(forAssetCollection: assetCollection) {
                     
-                    assetCollectionChangeRequest.addAssets(placeholders)
+                    assetCollectionChangeRequest.addAssets([ placeholder ])
                 }
             }
-            
-            }) { success, error  in
-                
-                completion(error: error)
         }
+    }
+    
+    class func filterAssetCollections(userCollections: [PHAssetCollection], forAlbumsInMetadata metadata: MVAssetSourceMetadata) -> [PHAssetCollection] {
+        
+        return userCollections.filter {
+            
+            return $0.localizedTitle != nil ? metadata.albums.contains($0.localizedTitle!) : false
+            
+        }
+    }
+    
+    class func fetchTopLevelUserCollections() -> [PHAssetCollection] {
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "localizedTitle", ascending: true)]
+        
+        let fetchResult = PHCollectionList.fetchTopLevelUserCollectionsWithOptions(fetchOptions)
+        
+        var collections: [PHAssetCollection] = []
+        fetchResult.enumerateObjectsUsingBlock { object, index, pointer in
+
+            if let collection = object as? PHAssetCollection {
+                collections.append(collection)
+            }
+        }
+        return collections
     }
 }
 
